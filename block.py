7e51243fb85a375e7c0188c5323ebf0fc572ad68
ying1xing <167100096+ying1xing@users.noreply.github.com>
2025-05-22 11:00:00 +0800

    Update block.py

--- block.py
+++ block.py
@@ -4,7 +4,8 @@ class Block:
         self.signers = []
         self.proposer = proposer
         self.committee = committee
-
+        self.transaction_count = 1  # 假设每个区块代表一次交易
+        
     def isValid(self):
         return True
 
